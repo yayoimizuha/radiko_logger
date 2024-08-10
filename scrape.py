e9929ffe9d4cb10810ef2cdf372298c5adfec337
yayoimizuha <poko.peko.tomo@icloud.com>
2024-08-10 19:21:34 +0900

    update

--- scrape.py
+++ scrape.py
@@ -1,19 +1,20 @@
 import time
 from os import path
-
+from settings import AUDIO_DIR
 from yt_dlp import YoutubeDL
 import re
 import requests
 import json
 
-DL_DIR = r"C:\Users\tomokazu\Music\radio"
 
 with open(file="key.json", mode="r", encoding="utf-8") as f:
     dl_key: dict = json.load(fp=f)
-radiko_api = ("https://radiko.jp/v3/api/program/search?key={}&filter=past&start_day=&end_day=&area_id=JP13&"
-              "region_id=all&cul_area_id=JP13&page_idx=0&uid=aa&row_limit=12&app_id=pc&cur_area_id=JP13&action_id=0")
+radiko_api = (
+    "https://radiko.jp/v3/api/program/search?key={}&filter=past&start_day=&end_day=&area_id=JP13&"
+    "region_id=all&cul_area_id=JP13&page_idx=0&uid=aa&row_limit=12&app_id=pc&cur_area_id=JP13&action_id=0"
+)
 print(dl_key)
-for (key, val) in dl_key.items():
+for key, val in dl_key.items():
     search_resp = requests.get(radiko_api.format(val)).json()
     for resp_val in search_resp["data"]:
         try:
@@ -25,7 +26,7 @@ for (key, val) in dl_key.items():
         page_id = "".join(re.findall(r"\d+", start_time))
         radiko_page_url = f"https://radiko.jp/#!/ts/{station_id}/{page_id}"
         print(radiko_page_url)
-        filename = path.join(DL_DIR, f"{key}_{page_id}.m4a")
+        filename = path.join(AUDIO_DIR, f"{key}_{page_id}.m4a")
         print(filename)
         if path.exists(filename):
             continue
---
+++ settings.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+from os import environ, path
+
+load_dotenv(verbose=True)
+
+dotenv_path = path.join(path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
+AUDIO_DIR = environ.get("AUDIO_DIR")
