e8ef8fc2c27743ab82774990b99010ac567c2eb9
yayoimizuha <poko.peko.tomo@icloud.com>
2024-08-12 01:38:09 +0900

    update prompt

--- gemini_hiraganize.py
+++ gemini_hiraganize.py
@@ -46,24 +46,20 @@ generation_config = {
 model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
     generation_config=generation_config,
+    system_instruction="""
+"入力の文章に含まれる漢字をすべて読み仮名に置き換え、ひらがなに変換して出力してください。変換した文章のみを出力しなさい。
+例:
+夏って本当に楽しいことがあるがゆえに体調なんて崩してられないよということで、体力作りと健康第一志向で頑張ってまいりましょう。 -> なつってほんとうにたのしいことがあるがゆえにたいちょうなんてくずしてられないよということで、たいりょくづくりとけんこうだいいちしこうでがんばってまいりましょう。
+PydubはPythonで音声処理を行うためのライブラリで、ステレオ音声をモノラルに変換するのも非常に簡単です。 => PydubはPythonでおんせいしょりをおこなうためのライブラリで、ステレオおんせいをモノラルにへんかんするのもひじょうにかんたんです。
+35キロ地点で、トラが2位に18秒差でトップを維持。そのままトラが逃げ切り、初制覇した。 -> 35キロちてんで、トラが2いに18びょうさでトップをいじ。そのままトラがにげきりはつせいはした。
+大学入試は多様化していて、初めて受験を考えるみなさんには複雑なシステムと感じるかもしれません。 -> だいがくにゅうしはたようかしていて、はじめてじゅけんをかんがえるみなさんにはふくざつなシステムとかんじるかもしれません。
+""",
 )
 
 with open(path.join(path.dirname(path_json), "hiraganized.txt"), mode="w") as fp:
-    for chunked_text in chunked(texts, n=60):
+    for chunked_text in chunked(texts, n=20):
         response = model.generate_content(
-            [
-                "入力の文章に含まれる漢字をすべて読み仮名に置き換え、ひらがなに変換して出力してください。変換した文章のみを出力しなさい。\n例:",
-                "input: 夏って本当に楽しいことがあるがゆえに体調なんて崩してられないよということで、体力作りと健康第一志向で頑張ってまいりましょう。",
-                "output: なつってほんとうにたのしいことがあるがゆえにたいちょうなんてくずしてられないよということで、たいりょくづくりとけんこうだいいちしこうでがんばってまいりましょう。",
-                "input: PydubはPythonで音声処理を行うためのライブラリで、ステレオ音声をモノラルに変換するのも非常に簡単です。",
-                "output: PydubはPythonでおんせいしょりをおこなうためのライブラリで、ステレオおんせいをモノラルにへんかんするのもひじょうにかんたんです。",
-                "input: 35キロ地点で、トラが2位に18秒差でトップを維持。そのままトラが逃げ切り、初制覇した。",
-                "output: 35キロちてんで、トラが2いに18びょうさでトップをいじ。そのままトラがにげきりはつせいはした。",
-                "input: 大学入試は多様化していて、初めて受験を考えるみなさんには複雑なシステムと感じるかもしれません。",
-                "output: だいがくにゅうしはたようかしていて、はじめてじゅけんをかんがえるみなさんにはふくざつなシステムとかんじるかもしれません",
-                "input: {}".format("".join(chunked_text)),
-                "output: ",
-            ],
+            "".join(chunked_text),
             safety_settings={
                 HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
                 HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
