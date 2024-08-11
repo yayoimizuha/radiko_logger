5516843747f2ea9e3780ffd4cfc1853ea11b6b70
yayoimizuha <poko.peko.tomo@icloud.com>
2024-08-11 23:35:00 +0900

    update

--- README.md
+++ README.md
@@ -6,10 +6,11 @@ find ~/Music/radio/ | xargs -P4 -IX --process-slot-var=DEVICE bash -c "CUDA_VISI
 ## `whisper-ctranslate2` for all files.
 ```bash
 cd separated/htdemucs
-{ find ./ -name "for_whisper.json" | xargs -n1 dirname | xargs -n1 basename ; ls -1 --color=none; } | sort | uniq -u | xargs -P4 -IX --process-slot-var=DEVICE bash -c "CUDA_VISIBLE_DEVICES=\$((\`printenv DEVICE\`%2)) whisper-ctranslate2 --model large-v3 --vad_filter True -f json -o X X/for_whisper.wav -p True"
+{ find ./ -name "for_whisper.json" | xargs -n1 dirname | xargs -n1 basename ; ls -1 --color=none; } | sort | uniq -u | xargs -P4 -IX --process-slot-var=DEVICE bash -c "CUDA_VISIBLE_DEVICES=\$((\`printenv DEVICE\`%2)) whisper-ctranslate2 --model large-v3 --language ja --vad_filter True -f json -o X X/for_whisper.wav -p True"
 ```
 
 ## `gemini_hiraganize.py` for all files.
 ```bash
-cd separated/htdemucs
-{ find ./ -name "for_whisper.json" | xargs -n1 dirname | xargs -n1 basename ; find ./ -name "hiraganized.txt" | xargs -n1 dirname | xargs -n1 basename ; } | sort | uniq -u | xargs -n1 python gemini_hiraganize.py
+# cd separated/htdemucs
+{ find ./ -name "for_whisper.json" | xargs -n1 dirname ; find ./ -name "hiraganized.txt" | xargs -n1 dirname ; } | sort | uniq -u | xargs -IX python gemini_hiraganize.py X/for_whisper.json
+```
--- gemini_hiraganize.py
+++ gemini_hiraganize.py
@@ -6,6 +6,7 @@ from more_itertools import chunked
 from sys import argv
 from json import load
 from jaconv import kata2hira
+
 genai.configure(api_key=GEMINI_API_KEY)
 
 
@@ -14,7 +15,25 @@ print(path_json)
 with open(file=path_json, mode="r", encoding="utf-8") as fp:
     transcribed_data = load(fp=fp)
 
+
 texts = [segment["text"] for segment in transcribed_data["segments"]]
+
+
+def remove_consecutive_duplicates(input_list: list[str]) -> list[str]:
+    if not input_list:
+        return []
+    # 結果リストの最初の要素に入力リストの最初の要素を追加
+    result_list = [input_list[0]]
+
+    for item in input_list[1:]:
+        # 前の要素と異なる場合にのみ結果リストに追加
+        if item != result_list[-1]:
+            result_list.append(item)
+
+    return result_list
+
+
+texts = remove_consecutive_duplicates(texts)
 
 generation_config = {
     "temperature": 0.6,
@@ -58,8 +77,8 @@ with open(path.join(path.dirname(path_json), "hiraganized.txt"), mode="w") as fp
         fp.flush()
 
 
-with open(path.join(path.dirname(path_json),"hiraganized.txt"),mode="r") as fp:
+with open(path.join(path.dirname(path_json), "hiraganized.txt"), mode="r") as fp:
     text_one_line = fp.read()
 
-with open(path.join(path.dirname(path_json),"hiraganized.txt"),mode="w") as fp:
-    fp.write(kata2hira(text_one_line.replace("\n","")))
+with open(path.join(path.dirname(path_json), "hiraganized.txt"), mode="w") as fp:
+    fp.write(kata2hira(text_one_line.replace("\n", "")))
